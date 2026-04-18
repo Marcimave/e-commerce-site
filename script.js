10ff1fc1a7852b879096f78ddcc2b4cc3c85f720
Marcimave <marcimavegithub@gmail.com>
2026-04-18 20:36:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+let total = 0;
 
+function ajouterPanier(produit, prix) {
+  total += prix;
+
+  document.getElementById("panier").innerHTML +=
+    produit + " - " + prix + "€<br>";
+
+  document.getElementById("panier").innerHTML +=
+    "<b>Total : " + total + "€</b><br><br>";
+}
